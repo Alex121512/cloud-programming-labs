10b8c07f8cc185162da006d28d7539e85ed6ee2a
Oleksandr <43420896+Alex121512@users.noreply.github.com>
2026-01-21 06:20:27 +0000

    TASK3: S3_FN_01: convert functions to arrows

---
+++ js/task3/S3_FN_01.js
@@ -0,0 +1,8 @@
+const square = (n) => n * n;
+const isOdd = (n) => n % 2 !== 0;
+const greet = (name) => `Hello, ${name}!`;
+
+// Tests
+console.log(square(4)); // 16
+console.log(isOdd(3)); // true
+console.log(greet("Ala")); // Hello, Ala!
